3e93437e56dcafe2ac0d917506329d5a662588b5
keonwookang0914 <kwkang0914@gmail.com>
2026-09-18 22:11:23 +0900

    Init Project

---
+++ ClayEngine/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <Windows.h>
+#include <crtdbg.h>
+
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "D3D12.lib")
+#pragma comment(lib, "dxgi.lib")
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, int showCmd)
+{
+    // Enable run-time memory check for debug builds.
+#if defined(DEBUG) | defined(_DEBUG)
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+    return 0;
+}
---
+++ Docs/reference.md
@@ -0,0 +1,7 @@
+# 사이트 모음
+
+### 마이크로 소프트 공식 D3D12 소개
+https://learn.microsoft.com/ko-kr/windows/win32/direct3d12/directx-12-programming-guide
+
+### microsoft DirectX Sample
+https://github.com/microsoft/DirectX-Graphics-Samples
